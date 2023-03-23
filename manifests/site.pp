0f3ce5a0e72ad98712af2e7ad9e80c98fc3e0639
root <root@docker-3.c.webforce3-378713.internal>
2023-03-23 15:20:15 +0000

    Ajout Modification manifest/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,16 @@
-if $osfamily == 'version_d'os trouve'{
-  include chrony
+exec { 'my_exec_command':
+  command => 'ls -alrt',
+  patch => '/user/bin:/usr/local/bin',
+  cwd => '/etc/puppetlasbs/puppet',
+  logoutput => true,
 }
+
+$result = []
+Exec['my_exec_command']->Notify[ 'result']
+
+notify{'results':
+  message => "c'est la directory puppet : ${result}",
+}
+
+$result = split(trim($::output),"\n")
+
